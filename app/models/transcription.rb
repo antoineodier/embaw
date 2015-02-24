430846085055dd40efc738d9069ebaf3c9be34af
antoineodier <odier@hab.de>
2015-02-24 13:44:36 +0100

    corrections-associations-sans2autresmodeles

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -1,5 +1,4 @@
 class Transcription < ActiveRecord::Base
   has_many :scans
-  has_many :users, through: :contribution_transcriptions
   validates :path_to_xml_file, uniqueness: true, presence: true
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,6 +1,5 @@
 class Translation < ActiveRecord::Base
   belongs_to :transcription
-  has_many :users, through: :contribution_translations
   validates :path_to_xml_file, presence: true
   validates :transcription, presence: true
 end
