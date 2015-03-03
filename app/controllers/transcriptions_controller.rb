253ac550daab305d47e116b60b5cf587d9913d67
antoineodier <odier@hab.de>
2015-03-03 02:13:55 +0100

    transformation-xsl-n_1

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -1,13 +1,16 @@
+require 'open-uri'
+
 class TranscriptionsController < ApplicationController
 
   def show
     @transcription = Transcription.find(params[:id])
+    @template = Nokogiri::XSLT(File.read('tei-transcript.xsl'))
   end
 
   private
 
     def transcription_params
-      params.require(:transcription).permit(:path_to_xml_file)
+      params.require(:transcription, :template).permit(:path_to_xml_file)
     end
 
 end
