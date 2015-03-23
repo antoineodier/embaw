d479d076ef9fb9df36d24f9f35002a8bcb3f2971
antoineodier <odier@hab.de>
2015-03-23 22:54:23 +0100

    ajax-page-1/2

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -6,13 +6,15 @@ class TranscriptionsController < ApplicationController
     @transcription = Transcription.find(params[:id])
     @document = Nokogiri::XML(@transcription.xml_content)
     @template = Nokogiri::XSLT(File.read('tei-transcript-simple.xsl'))
-    @transformed_document = @template.transform(@document)
+    @transformed_document = @template.transform(@document).css("body").to_s
     @scan = Scan.find(3)
     @array_page_numbers = array_page_numbers
-    if @displayed_page == nil
-      @displayed_page = 0
-    end
+    @array_pages_html = array_pages_html
+    @first_loaded_page = @array_pages_html[0]
+    response_ajax
   end
+
+end
 
   private
 
@@ -28,4 +30,31 @@ class TranscriptionsController < ApplicationController
       return array
     end
 
-end
+    def response_ajax
+      respond_to do |format|
+        format.html
+        format.json { render :json => @array_page_numbers }
+      end
+    end
+
+    def array_pages_html
+      array_pages_html_contenu = []
+      @array_page_numbers.each_with_index do |page_number, index|
+        if page_number != @array_page_numbers.last
+          array_pages_html_contenu << display_page(index)
+        elsif page_number == @array_page_numbers.last
+          array_pages_html_contenu << display_last_page(index)
+        end
+      end
+      return array_pages_html_contenu
+    end
+
+    def display_page(page_number)
+      "<br><i><b>Pagination manuscrit : #{@array_page_numbers[page_number.to_i]}</b></i><br>" + @transformed_document.to_s[/#{Regexp.escape("<br><i><b>Pagination manuscrit : #{@array_page_numbers[page_number.to_i]}</b></i>")}(.*?)#{Regexp.escape("<br><i><b>Pagination manuscrit : #{@array_page_numbers[page_number.to_i + 1]}</b></i>")}/m, 1]
+    end
+
+    def display_last_page(page_number)
+      "<br><i><b>Pagination manuscrit : #{@array_page_numbers[page_number.to_i]}</b></i><br>" + @transformed_document.to_s[/#{Regexp.escape("<br><i><b>Pagination manuscrit : #{@array_page_numbers[page_number.to_i]}</b></i>")}(.*?)/m, 1]
+    end
+
+
