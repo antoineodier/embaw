0ed3f478e567a732b90af41685aa0c5927b781f4
antoineodier <odier@hab.de>
2015-03-23 05:11:35 +0100

    array_page_numbers

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -8,12 +8,22 @@ class TranscriptionsController < ApplicationController
     @template = Nokogiri::XSLT(File.read('tei-transcript-simple.xsl'))
     @transformed_document = @template.transform(@document)
     @scan = Scan.find(3)
+    @array_page_numbers = array_page_numbers
+
   end
 
   private
 
     def transcription_params
       params.require(:transcription, :template).permit(:path_to_xml_file)
+    end
+
+    def array_page_numbers
+      array=[]
+      @document.search("pb").each do |element|
+        array << element.first[1]
+      end
+      return array
     end
 
 end
