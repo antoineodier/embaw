ffeb7b888b3dee8184b4b9efca6dc107bf08131d
antoineodier <odier@hab.de>
2015-04-04 00:11:42 +0200

    fichier-seed-5 transcriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# 1/Jean Maillefer
+Transcription.create(
+  path_to_diplomatic_transcription: "jean_maillefer_registre/diplomatic_transcription-jean_maillefer.xml",
+  path_to_normalized_transcription: "jean_maillefer_registre/normalized_transcription-jean_maillefer.xml"
+  path_to_diplomatic_transformation: "jean_maillefer_registre/diplomatic_transformation-jean_maillefer.xsl",
+  path_to_normalized_transformation: "jean_maillefer_registre/normalized_transformation-jean_maillefer.xsl"
+)
+
+# 2/Isaac Bostaquet
+Transcription.create(
+  path_to_diplomatic_transcription: "isaac_bostaquet_registre/diplomatic_transcription-isaac_bostaquet.xml",
+  path_to_normalized_transcription: "isaac_bostaquet_registre/normalized_transcription-isaac_bostaquet.xml"
+  path_to_diplomatic_transformation: "isaac_bostaquet_registre/diplomatic_transformation-isaac_bostaquet.xsl",
+  path_to_normalized_transformation: "isaac_bostaquet_registre/normalized_transformation-isaac_bostaquet.xsl"
+)
+
+# 3/Glikl Hameln
+Transcription.create(
+  path_to_diplomatic_transcription: "glikl_hameln_zikhronot/diplomatic_transcription-glikl_hameln.xml",
+  path_to_normalized_transcription: "glikl_hameln_zikhronot/normalized_transcription-glikl_hameln.xml"
+  path_to_diplomatic_transformation: "glikl_hameln_zikhronot/diplomatic_transformation-glikl_hameln.xsl",
+  path_to_normalized_transformation: "glikl_hameln_zikhronot/normalized_transformation-glikl_hameln.xsl"
+)
+
+# 4/Augustin Güntzer
+Transcription.create(
+  path_to_diplomatic_transcription: "augustin_guentzer_biechlin/diplomatic_transcription-augustin_guentzer.xml",
+  path_to_normalized_transcription: "augustin_guentzer_biechlin/normalized_transcription-augustin_guentzer.xml"
+  path_to_diplomatic_transformation: "augustin_guentzer_biechlin/diplomatic_transformation-augustin_guentzer.xsl",
+  path_to_normalized_transformation: "augustin_guentzer_biechlin/normalized_transformation-augustin_guentzer.xsl"
+)
+
+# 5/Matheus Miller
+Transcription.create(
+  path_to_diplomatic_transcription: "matheus_miller_aufzeichnungen/diplomatic_transcription-matheus_miller.xml",
+  path_to_normalized_transcription: "matheus_miller_aufzeichnungen/normalized_transcription-matheus_miller.xml"
+  path_to_diplomatic_transformation: "matheus_miller_aufzeichnungen/diplomatic_transformation-matheus_miller.xsl",
+  path_to_normalized_transformation: "matheus_miller_aufzeichnungen/normalized_transformation-matheus_miller.xsl"
+)
