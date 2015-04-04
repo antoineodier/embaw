fe7716e88f4416b755d6b8b86ea788ee8007036b
antoineodier <odier@hab.de>
2015-04-04 02:12:56 +0200

    transcription.id = 11 pour la page d'accueil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    transcription_path(1)
+    transcription_path(11)
   end
 
   def user_not_authorized
