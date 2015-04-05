a03b66f2c678cdf7f2885d89bd3f13bd9df45607
antoineodier <odier@hab.de>
2015-04-05 16:37:59 +0200

    nvelle_route-transcriptions#submit_correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   resources :transcriptions, only: [:show]
+  get "/transcriptions/submit_correction", to: 'transcriptions#submit_correction'
 end
